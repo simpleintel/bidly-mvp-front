ff3c203ff91385e4e0d11f8cf7817647d8d69617
declandev <declandevwork@gmail.com>
2023-08-11 21:35:32 +0800

    fix multiple render and messag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,43 @@
 import { useEffect, useState } from "react"
 import React from 'react';
-
+import axios from 'axios'
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, message, Upload, notification } from 'antd';
-import axios from 'axios'
 import { Image, Spin } from 'antd';
 
 const IndexPage = () => {
   const [pdfFile, setPdfFile] = useState([])
   const [fileList, setFileList] = useState([])
   const [messageSuccess, setMessageSuccess] = useState(null)
+  const [displayMessage, setdisplayMessage] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [api, contextHolder] = notification.useNotification();
+  const [uploadInProgress, setUploadInProgress] = useState(false);
 
   const props = {
 
-    beforeUpload:(file) => {
+    beforeUpload(file) {
+
       const isPNG = file.type === 'application/pdf';
       if (!isPNG) {
         message.error(`${file.name} is not a PDF file`);
       }
       return isPNG || Upload.LIST_IGNORE;
     },
-    onChange: (info) => {
-      if (info.fileList.length > 0) {
+    onChange(info) {
+      if (uploadInProgress) {
+        // Ignore onChange while an upload is already in progress
+        return;
+      }
+
+      setUploadInProgress(true);
+      setIsLoading(true)
+
+      if (!uploadInProgress && info.fileList.length > 0) {
         const formData = new FormData();
         formData.append('file', info.fileList[0].originFileObj);
-        setIsLoading(true)
         if (info) {
-  
+
           axios.post('http://ec2-13-53-130-22.eu-north-1.compute.amazonaws.com:5000/upload', formData, {
             headers: {
               'Content-Type': 'multipart/form-data',
@@ -36,22 +45,24 @@ const IndexPage = () => {
             }
           }).then((response) => {
             setMessageSuccess(response.data.issuccess)
-        setIsLoading(false)
-      setPdfFile(response.data.results)
+            setIsLoading(false)
+            setPdfFile(response.data.results)
+            setdisplayMessage(response.data.success_message)
 
-  
           }).catch((error) => {
+            console.log('error', error)
+            setdisplayMessage(response.data.error_message)
             setMessageSuccess(false)
             setIsLoading(false)
             setFileList(info.fileList.filter((file) => file !== info.fileList[0]));
-           props.beforeUpload(info.fileList[0]);
+            props.beforeUpload(info.fileList[0]);
           });
         }
       }
     }
-  
+
   }
-  
+
   useEffect(() => {
     if (messageSuccess) {
       openNotificationWithIcon('success')
@@ -69,7 +80,7 @@ const IndexPage = () => {
 
   const openNotificationWithIcon = (type) => {
     api[type]({
-      message: messageSuccess ? 'Successfully received files' : 'Error in PDF Upload',
+      message: displayMessage,
       description:
         '',
     });
@@ -84,15 +95,15 @@ const IndexPage = () => {
           Upload PDF File
           <br />
         </h1>
-      <div style={{ width: '200px', margin: 'auto' }}>
+        <div style={{ width: '200px', margin: 'auto' }}>
 
-      <Upload  {...props} onRemove={onRemove} maxCount={1}>
-          <Button disabled={isLoading} icon={<UploadOutlined />}>Upload PDF only</Button>
-        </Upload>
-        
-        { isLoading &&<Spin style={{ marginTop: '24px' }} size="large" /> }
-    
-      </div>
+          <Upload  {...props} onRemove={onRemove} maxCount={1}>
+            <Button disabled={isLoading} icon={<UploadOutlined />}>Upload PDF only</Button>
+          </Upload>
+
+          {isLoading && <Spin style={{ marginTop: '24px' }} size="large" />}
+
+        </div>
       </div>
 
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '70%', margin: 'auto' }} >
