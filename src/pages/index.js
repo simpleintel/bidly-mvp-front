fc17316bb74293a5b1c2c6ae57731b60ebad0004
R G <rsg@Rs-MacBook-Pro.local>
2024-03-19 13:41:35 -0500

    update page link

--- src/pages/index.js
+++ src/pages/index.js
@@ -203,6 +203,7 @@ const IndexPage = () => {
                       />
                       <p><span style={{ fontWeight: 'bold' }}>Number of Cabinets:</span> {pdfDisplayList.num_bath_cabinets[index]}</p>
                       <p><span style={{ fontWeight: 'bold' }}>Number of Page:</span> {pdfDisplayList.page_num}</p>
+                      <a href={pdfDisplayList.page_urls} target="_blank"><span style={{ fontWeight: 'bold' }}>Open Page</span></a>
                     </div>
                   </div>
                 ))}
